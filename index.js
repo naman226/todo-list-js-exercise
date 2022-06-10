afb7a6ca8f4dcc2290212fd89420d48c86c715f9
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-10 21:53:49 +0000

    Fix spacing

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
 
 
-function newTask(title, description) {
+const newTask = function(title, description) {
   const task = {
-  title: title,
-  description: description,
-  complete: false,
-  markCompleted: function() {
-    task.complete = true;
-  },
-  logState: function() {
+    title: title,
+    description: description,
+    complete: false,
+    markCompleted: function() {
+      task.complete = true;
+    },
+    logState: function() {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     }
-  }
+  };
   return task;
 };
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-const task2 = newTask("Do Laundry", "😨");
+
 
 task1.logState();
 task1.markCompleted();
